a44614b37a970214291bbec745a58aa2cecab19d
Michele <michele.pellegrino@scilifelab.se>
2025-04-30 16:47:31 +0200

    Checking if mpi4py is using the correct backend

--- test-lammps.py
+++ test-lammps.py
@@ -2,8 +2,12 @@ import lammps
 
 # These are the flags needed to run on GPU with Kokkos
 lmp = lammps.lammps(cmdargs=['-k','on','g','1','-sf','kk'])
-
 lmp.close()
 lmp.finalize()
+
+# Testing if mpi4py is using the correct backend
+from mpi4py import MPI
+print("MPI vendor:", MPI.get_vendor())
+print("MPI version:", MPI.Get_version())
 
 print("PASSED")
